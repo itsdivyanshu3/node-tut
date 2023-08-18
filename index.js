4664399c26409d0b36e82425603b491518536fd6
Divyanshu Sharma <divyanshushrma1@gmail.com>
2023-08-18 22:27:19 +0530

    few changes

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ console.log("hello");
 const color = require('colors')
 
 console.log("Divyashu's First Program".bgRed)
-console.log("Node Js first Program".green)
+console.log("Node Js first Program".bgGreen)
